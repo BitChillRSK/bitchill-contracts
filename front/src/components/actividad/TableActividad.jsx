b696d24d31c8174fa8a329429c4098a11e4fc905
Pau MB <Pau MB>
2024-02-12 20:06:25 +0100

    past events

--- front/src/components/actividad/TableActividad.jsx
+++ front/src/components/actividad/TableActividad.jsx
@@ -45,15 +45,18 @@ const rows = [
 const DCA_ADDRESS = '0x322D577d1db3Be7151BC547409780676a59a0E75';
 const DOC_ADDRESS = '0xCb46C0DdC60d18eFEB0e586c17AF6Ea36452DaE0';
 
-const EVENT_NAME_BUY = 'PurchasePeriodSet';
+const EVENT_NAME_BUY = 'RbtcBought';
 
 export default function TableActividad() {
 	const { provider } = useContext(Web3Context);
 
-	useEffect(() => {
+	/* useEffect(() => {
 		const getEventsDCAEthers = async () => {
 			const provider3 = new ethers.providers.Web3Provider(provider);
-			const dcaContract = new ethers.Contract(DCA_ADDRESS, ABI_DCA, provider3);
+			const dcaContract = new ethers.Contract(
+				DCA_ADDRESS,
+				ABI_DCA
+			);
 			const rbtcBoughtListener = (user, docAmount, rbtcAmount, event) => {
 				const data = {
 					user,
@@ -63,17 +66,17 @@ export default function TableActividad() {
 				console.log(`event ${EVENT_NAME_BUY}`, data);
 			};
 
-			dcaContract.on('EVENT_NAME_BUY', rbtcBoughtListener);
-
+			dcaContract.on(EVENT_NAME_BUY, rbtcBoughtListener);
 			return () => {
 				dcaContract.off(EVENT_NAME_BUY, rbtcBoughtListener);
 			};
 		};
 
-		/**
-		 * web3
-		 */
-		// getEventsDCAEthers();
+		getEventsDCAEthers();
+	}, []); */
+
+	// web3
+	useEffect(() => {
 		const getPastEventsDCAWeb3 = async () => {
 			const web3 = new Web3(provider);
 			const dcaContract = new web3.eth.Contract(ABI_DCA, DCA_ADDRESS);
@@ -84,31 +87,29 @@ export default function TableActividad() {
 						toBlock: n,
 					})
 					.then(event => console.log(`Eventos ${EVENT_NAME_BUY}`, event))
-					.catch(() => console.log('error when get past events'));
+					.catch(error => console.log('error when get past events', error));
 			});
 		};
-		// getPastEventsDCAWeb3();
+		getPastEventsDCAWeb3();
+	}, []);
 
+	/* useEffect(() => {
 		const getEventsDoC = async () => {
-			try {
-				const web3 = new Web3(provider);
-				const dcaContract = new web3.eth.Contract(ABI_APPROVE, DOC_ADDRESS);
-				web3.eth.getBlockNumber().then(n => {
-					dcaContract
-						.getPastEvents('Transfer', {
-							fromBlock: 0,
-							toBlock: n,
-						})
-						.then(event => console.log(`Eventos Transfer`, event))
-						.catch(() => console.log('yep'));
-				});
-			} catch (err) {
-				console.log('hay errores');
-			}
+			const web3 = new Web3(provider);
+			const dcaContract = new web3.eth.Contract(ABI_APPROVE, DOC_ADDRESS);
+			web3.eth.getBlockNumber().then(n => {
+				dcaContract
+					.getPastEvents('Transfer', {
+						fromBlock: 0,
+						toBlock: n,
+					})
+					.then(event => console.log(`Eventos Transfer`, event))
+					.catch(() => console.log('yep'));
+			});
 		};
 
 		getEventsDoC();
-	}, []);
+	}, []); */
 
 	return (
 		<TableContainer component={Paper}>
