668b92f8295e5969398a11a3deb809fb6ebb24fa
Ynyesto <arynyestos@gmail.com>
2025-03-07 18:46:32 +0100

    Cleaned up comments

--- smart-contracts/src/TokenHandler.sol
+++ smart-contracts/src/TokenHandler.sol
@@ -20,15 +20,7 @@ abstract contract TokenHandler is ITokenHandler, ERC165, Ownable, FeeHandler, Dc
     // State variables ///
     //////////////////////
     uint256 internal s_minPurchaseAmount; // The minimum amount of this token for periodic purchases
-    // mapping(address user => uint256 amount) internal s_usersAccumulatedRbtc;
-    // uint256 constant FEE_PERCENTAGE_DIVISOR = 10_000; // feeRate will belong to [100, 200], so we need to divide by 10,000 (100 * 100)
-    // address public immutable i_stableToken; // The stablecoin token to be deposited
     IERC20 public immutable i_stableToken; // The stablecoin token to be deposited
-
-    // i_yieldsInterest doesn't seem necessary anymore. TODO: REMOVE!!!
-
-    // Store user DCA details generically
-    // mapping(address => DcaDetails) public dcaDetails;
 
     constructor(
         address dcaManagerAddress,
@@ -37,7 +29,7 @@ abstract contract TokenHandler is ITokenHandler, ERC165, Ownable, FeeHandler, Dc
         address feeCollector,
         FeeSettings memory feeSettings
     ) FeeHandler(feeCollector, feeSettings) DcaManagerAccessControl(dcaManagerAddress) {
-        i_stableToken = IERC20(tokenAddress); // TODO: remove this parameter!!
+        i_stableToken = IERC20(tokenAddress); // TODO: remove this parameter!! (can't remember why, potentially: TODO remove comment)
         s_minPurchaseAmount = minPurchaseAmount;
         s_feeCollector = feeCollector;
         s_minFeeRate = feeSettings.minFeeRate;
@@ -50,39 +42,30 @@ abstract contract TokenHandler is ITokenHandler, ERC165, Ownable, FeeHandler, Dc
                            EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
 
-    receive() external payable /*onlyMocProxy*/ {} // Cambiar onlyMocProxy por algo que controle que el rbtc venga de fuentes conocidas?
+    receive() external payable /*onlyMocProxy*/ {} // TODO? Cambiar onlyMocProxy por algo que controle que el rbtc venga de fuentes conocidas?
 
     /**
      * @notice deposit the full token amount for DCA on the contract
+     * @notice This function transfers the selected token from the user to this contract. The user must have called the token contract's
+     * approve function with this contract's address and the amount approved
      * @param user: the address of the user making the deposit
      * @param depositAmount: the amount to deposit
      */
     function depositToken(address user, uint256 depositAmount) public virtual override onlyDcaManager {
-        // Transfer the selected token from the user to this contract. The user must have called the token contract's
-        // approve function with this contract's address and the amount approved
-        if (IERC20(i_stableToken).allowance(user, address(this)) < depositAmount) {
+        if (i_stableToken.allowance(user, address(this)) < depositAmount) {
             revert TokenHandler__InsufficientTokenAllowance(address(i_stableToken));
         }
-
-        IERC20(i_stableToken).safeTransferFrom(user, address(this), depositAmount);
-
-        // bool depositSuccess = IERC20(i_stableToken).safeTransferFrom(user, address(this), depositAmount);
-        // if (!depositSuccess) revert TokenHandler__TokenDepositFailed(i_stableToken);
-
+        i_stableToken.safeTransferFrom(user, address(this), depositAmount);
         emit TokenHandler__TokenDeposited(address(i_stableToken), user, depositAmount);
     }
 
     /**
      * @notice withdraw some or all of the DOC previously deposited
+     * @notice This function transfers DOC from this contract back to the user
      * @param withdrawalAmount: the amount of DOC to withdraw
      */
     function withdrawToken(address user, uint256 withdrawalAmount) public virtual override onlyDcaManager {
-        // Transfer DOC from this contract back to the user
-        IERC20(i_stableToken).safeTransfer(user, withdrawalAmount);
-
-        // bool withdrawalSuccess = IERC20(i_stableToken).safeTransfer(user, withdrawalAmount);
-        // if (!withdrawalSuccess) revert TokenHandler__TokenWithdrawalFailed(i_stableToken);
-
+        i_stableToken.safeTransfer(user, withdrawalAmount);
         emit TokenHandler__TokenWithdrawn(address(i_stableToken), user, withdrawalAmount);
     }
 
